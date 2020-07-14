ff30d640bea50c57451bf643f5c7990ea115584c
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 20:44:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,21 @@
 class Artist
   @@artistall = [ ]
+  artistcounter = 0
   attr_accessor :name
   
   def initialize(name)
     @name = name
     @@artistall << self
+    artistcounter += 1
     
   end
   
   def add_song(song1)
     song1.artist == self
   end
+  
+  def self.all
+    
     
   
   
