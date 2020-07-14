559b0329e9b3f01bab2c4ab125529beb3e36ca13
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 21:40:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   
   def artist_name= (name1)
     self.artist = Artist.find_or_create_by_name(name1)
-    @artist = self.artist
+    Artist.add_song(self)
   end
   
    def self.new_by_filename(file)
