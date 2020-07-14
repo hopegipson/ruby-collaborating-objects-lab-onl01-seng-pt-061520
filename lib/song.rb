03f442e45f5c94b3350104384da1d5a569369e85
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 21:51:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,12 @@
 class Song
   
   attr_accessor :artist, :name
-  @@all = [ ]
-  
+
   def initialize(name)
     @name = name
-    @@all << self
   end
   
-  def self.all
-    @@all
-  end
+
 
   
    def self.new_by_filename(file)
