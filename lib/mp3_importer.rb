30e488709196d3e47fa663c04e65635b42b09092
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 22:06:51 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,6 +5,10 @@ class MP3Importer
    @path = path
  end
  
+  def files
+      @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+    end
+ 
   def import
     self.files.each{ |filename| Song.new_by_filename(filename) }
   end
