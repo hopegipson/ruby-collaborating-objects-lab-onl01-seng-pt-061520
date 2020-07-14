dc010d81892a17f97244372686d796d32440c099
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 20:49:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,14 +12,6 @@ class Song
     @@all
   end
   
-  def artist_name
-    if self.artist
-    self.artist.name
-    else
-    nil
-    end
-  end
-  
   def artist_name=(name)
     if (self.artist.nil?)
       self.artist = Artist.new(name)
