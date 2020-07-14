5f11fca47f84e0282d9f93a4f5c7e1a8d0d5fd52
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 21:48:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,7 @@ class Song
     @@all
   end
   
-  def artist_name= (name1)
-    self.artist = Artist.find_or_create_by_name(name1)
-    self.artist.add_song(self)
-  end
+
   
    def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
